f2e15dc606500f6c7be1ff516111779549c0b697
anasirfan <39003284+anasirfan@users.noreply.github.com>
2024-02-05 19:13:47 +0500

    Update page.tsx

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -46,8 +46,8 @@ export default async function Page({ params: { slug = 'home' } }) {
   // you should delete this code once you have a home page in the CMS
   // this is really only useful for those who are demoing this template
   if (!page && slug === 'home') {
-    // page = staticHome
-    return notFound()
+    page = staticHome
+    // return notFound()
   }
 
   if (!page) {
